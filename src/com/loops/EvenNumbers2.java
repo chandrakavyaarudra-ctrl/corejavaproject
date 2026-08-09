05337d5387fe270a54a1e6bb89ee9a3dbb9564c5
chandrakavyaarudra-ctrl <chandrakavyaarudra@gmail.com>
2026-08-09 11:58:16 +0530

    loops

---
+++ src/com/loops/EvenNumbers2.java
@@ -0,0 +1,12 @@
+package com.loops;
+
+public class EvenNumbers2 {
+
+	public static void main(String[] args) {
+		System.out.println("main method started..");
+		for(int i = 100; i >= 0; i = i - 1) {
+			System.out.println(i + " ");
+		}
+	}
+
+}
