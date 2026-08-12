0f582b50467cee3e90d5c23662036803754e7cce
chandrakavyaarudra-ctrl <chandrakavyaarudra@gmail.com>
2026-08-12 22:28:51 +0530

    arrays program

--- src/com/Arrays/Searching.java
+++ src/com/Arrays/Searching.java
@@ -6,26 +6,26 @@ public class Searching {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr=new int[5];
+		int[] arr = new int[5];
 		System.out.println("Enter 5 values : ");
-		for(int i = 0; i<arr.length;i++) {
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
-			
+
 		}
 		System.out.println("Enter search element : ");
 		int search = sc.nextInt();
 		boolean found = false;
-		for(int i =0;i<arr.length;i++) {
-			if(arr[i]==search) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == search) {
 				System.out.println(search + "is present at index " + i);
 				found = true;
 				break;
 			}
 		}
-		if(!found) {
+		if (!found) {
 			System.out.println(search + "is not present");
 		}
-		
+
 	}
 
 }
